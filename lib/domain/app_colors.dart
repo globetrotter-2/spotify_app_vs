d655e76d3ae71f906b7c5e87443c9df0aefe30f5
Karthikeyan <karthikeyansubramani344@gmail.com>
2025-04-09 12:48:12 +0530

    Reinitializing Git and first push

---
+++ lib/domain/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static final Color primaryColor = Color(0xff1ED760);
+  static final Color blackColor = Color(0xff121212);
+  static final Color secondaryBlackColor = Color(0xff282828);
+  static final Color greyColor = Color(0xff777777);
+  static final Color whiteColor = Color(0xffFFFFFF);
+}
